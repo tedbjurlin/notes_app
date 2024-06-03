b01e93d258f49e1d7b249dce4aa498447a9bcb03
= <=>
2024-06-03 12:55:11 +0200

    Added Cargo.lock

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e; set -o pipefail;
 
-nix build '.#docker'
+nix build '.#docker' --show-trace
 image=$((docker load < result) | sed -n '$s/^Loaded image: //p')
 docker image tag "$image" rust-template:latest
